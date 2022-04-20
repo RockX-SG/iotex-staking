7ce2a167ee8e179de9d48b1fe9cf2f14bdf0372a
xtaci <xtaci@Lis-MacBook-Air.local>
2022-04-20 14:59:21 +0800

    add deploy script

--- scripts/ganache_deploy.py
+++ scripts/ganache_deploy.py
@@ -1,6 +1,8 @@
 from brownie import *
 from pathlib import Path
 
+GAS_LIMIT = 6721975
+
 def main():
     deps = project.load(  Path.home() / ".brownie" / "packages" / config["dependencies"][0])
     TransparentUpgradeableProxy = deps.TransparentUpgradeableProxy
@@ -9,13 +11,42 @@ def main():
     deployer = accounts[1]
     print(f'contract owner account: {owner.address}\n')
 
-    stIOTEX_contract = stIOTEX.deploy(
+    stIOTX_contract = stIOTX.deploy(
             {'from': deployer}
             )
 
-    stIOTEX_proxy = TransparentUpgradeableProxy.deploy(
-            stIOTEX_contract, deployer, b'',
+    stIOTX_proxy = TransparentUpgradeableProxy.deploy(
+            stIOTX_contract, deployer, b'',
             {'from': deployer}
             )
 
+    iotexStaking_contract = IOTEXStaking.deploy(
+            {'from': deployer}
+            )
 
+    iotexStaking_proxy = TransparentUpgradeableProxy.deploy(
+            iotexStaking_contract, deployer, b'',
+            {'from': deployer}
+            )
+
+    transparent_stIOTX= Contract.from_abi("stIOTX", stIOTX_proxy.address, stIOTX.abi)
+    transparent_staking = Contract.from_abi("IOTEXStaking", iotexStaking_proxy.address, IOTEXStaking.abi)
+
+
+    transparent_stIOTX.initialize(
+            {'from': owner, 'gas': GAS_LIMIT}
+            )
+
+    transparent_stIOTX.setMintable(
+            transparent_staking, True,
+            {'from': owner, 'gas': GAS_LIMIT}
+            )
+
+    transparent_staking.initialize(
+            {'from': owner, 'gas': GAS_LIMIT}
+            ) 
+
+    transparent_staking.setStIOTXContractAddress(
+            transparent_stIOTX,
+            {'from': owner, 'gas': GAS_LIMIT}
+            )
